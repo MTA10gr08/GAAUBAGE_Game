9ec529eb28bb40b04605506b54d08f0073344438
Martin Dahl Sørensen <Zexyqag@gmail.com>
2023-04-13 14:50:24 +0200

    Update ContextCassification.cs, SegmentationService.cs, and 3 more files...

---
+++ Assets/Scripts/API/Services/SegmentationService.cs
@@ -0,0 +1,35 @@
+﻿using GAAUBAGE_Game.API.Models;
+using GAAUBAGE_Game.API.Networking;
+using System;
+using System.Threading.Tasks;
+
+#nullable enable
+namespace GAAUBAGE_Game.API.Services
+{
+    internal class SegmentationService
+    {
+        public static async Task<RequestResult<Segmentation>> PostSegmentationAsync(Segmentation Segmentation)
+        {
+            string apiUrl = Endpoints.Users.Post();
+            return await APIRequestHandler.PostAsync<Segmentation, Segmentation>(apiUrl, Segmentation);
+        }
+
+        public static void PostSegmentation(SegmentationService user, Action<RequestResult<Segmentation>>? onResponse = null)
+        {
+            string apiUrl = Endpoints.Segmentation.Post();
+            APIRequestHandler.Post(apiUrl, user, onResponse);
+        }
+
+        public static async Task<RequestResult<TrashCategory>> NextSegmentationAsync()
+        {
+            string apiUrl = Endpoints.Segmentation.Next();
+            return await APIRequestHandler.GetAsync<TrashCategory>(apiUrl);
+        }
+
+        public static void GetSegmentation(Action<RequestResult<TrashCategory>>? onResponse = null)
+        {
+            string apiUrl = Endpoints.Segmentation.Next();
+            APIRequestHandler.Get(apiUrl, onResponse);
+        }
+    }
+}
