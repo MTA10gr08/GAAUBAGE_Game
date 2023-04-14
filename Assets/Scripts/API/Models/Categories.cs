5b1fd4c4a2b0b30df265cbd45ff5510213981098
Martin Dahl Sørensen <Zexyqag@gmail.com>
2023-04-14 10:22:44 +0200

    Update Categories.cs, ContextCategories.cs, and 3 more files...

---
+++ Assets/Scripts/API/Models/Categories.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAAUBAGE_Game.Scripts.API.Models
+{
+    internal class Categories
+    {
+        public string[] BackgroundCategories { get; set; }
+        public string[] ContextCategories { get; set; }
+        public TrashCategory[] TrashCategories { get; set; }
+
+        public partial class TrashCategory
+        {
+            public string Name { get; set; }
+            public string[] SubCategories { get; set; }
+        }
+    }
+}
