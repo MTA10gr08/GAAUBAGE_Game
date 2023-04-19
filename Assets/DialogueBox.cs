9e8d51f39d0dfd5947c0825bd3036825db3be804
Marcus <Marcus@PP.local>
2023-04-19 10:55:43 +0200

    Update Skip test.asset, Skip test.asset.meta, and 6 more files...

--- Assets/DialogueBox.cs
+++ Assets/DialogueBox.cs
@@ -3,38 +3,48 @@ using UnityEngine;
 
 public class DialogueBox : MonoBehaviour
 {
-	TMPro.TMP_Text txt;
-	int index = 0;
-	public ConversationScriptableObject convo;
-	bool skipping;
+    public TMPro.TMP_Text speechBox, nameBox;
+    int index = 0;
+    public ConversationScriptableObject convo;
+    bool writing;
+    Coroutine writer;
 
-	void Awake() {
-		txt = GetComponent<TMPro.TMP_Text>();
-		story = txt.text;
-		txt.text = "";
+    void Awake() {
+        //speech = GetComponent<TMPro.TMP_Text>();
+        speechBox.text = "";
+        nameBox.text = "";
+    }
 
-		//NextDialogue();
-	}
-	IEnumerator PlayText() {
-		foreach (char c in convo.Dialogues[index].speech) {
-			txt.text += c;
-            if (skipping) {
-				yield return null;
-            } else {
-				yield return new WaitForSeconds(convo.Dialogues[index].TextSpeed);
-            }
-		}
-		index++;
-	}
-	[ContextMenu("NextDialogue")]
-	void NextDialogue()
-    {
-        if (index >= convo.Dialogues.Count) {
-			//Finish Convo if no more dialogue
-			return;
+    IEnumerator PlayText() {
+        foreach (char c in convo.Dialogues[index].speech) {
+            speechBox.text += c;
+            yield return new WaitForSeconds(convo.Dialogues[index].TextSpeed);
         }
-		txt.text = convo.Dialogues[index].name + ": ";
-		StartCoroutine(PlayText());
+        index++;
+        writing = false;
+    }
+
+    [ContextMenu("NextDialogue")]
+    public void NextDialogue() {
+        if (index >= convo.Dialogues.Count) {
+            //Finish Convo if no more dialogue
+            return;
+        }
+        nameBox.text = convo.Dialogues[index].name;
+
+        if (writing) {
+            StopCoroutine(writer);
+            speechBox.text = convo.Dialogues[index].speech;
+            index++;
+            writing = false;
+            return;
+        }
+
+
+
+        writing = true;
+        speechBox.text = "";
+        writer = StartCoroutine(PlayText());
     }
 
 }
