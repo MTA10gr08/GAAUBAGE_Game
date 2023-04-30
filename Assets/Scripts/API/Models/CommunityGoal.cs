904865f3fe77a296c801dfce2bbd14a2ca2272fd
Martin Dahl Sørensen <Zexyqag@gmail.com>
2023-04-30 12:56:37 +0200

    Update CommunityGoal.cs, DatabaseInfo.cs, and 4 more files...

--- Assets/Scripts/API/Models/CommunityGoal.cs
+++ Assets/Scripts/API/Models/CommunityGoal.cs
@@ -2,6 +2,9 @@ namespace GAAUBAGE_Game.API.Models
 {
     public class CommunityGoal
     {
-
+        public string TaskType { get; set; } = string.Empty;
+        public uint TotalToDo;
+        public uint DoneAll;
+        public uint DoneYou;
     }
 }
--- Assets/Scripts/API/Models/DatabaseInfo.cs
+++ Assets/Scripts/API/Models/DatabaseInfo.cs
@@ -1,13 +1,17 @@
+using static Unity.Collections.NativeArray<T>;
+
 namespace GAAUBAGE_Game.API.Models
 {
     public class DatabaseInfo
     {
         public uint TotalImages { get; set; }
+        public uint TotalSkipped { get; set; }
+        public uint TotalSubImages { get; set; }
         public uint TotalBackgroundClassified { get; set; }
         public uint TotalContextClassified { get; set; }
-        public uint TotalTrashBoundingBoxed { get; set; }
+        public uint TotalSubImaged { get; set; }
         public uint TotalTrashSuperCategorised { get; set; }
-        public uint TotalTrashCategorised { get; set; }
+        public uint TotalTrashSubCategorised { get; set; }
         public uint TotalSegmentated { get; set; }
     }
 }
