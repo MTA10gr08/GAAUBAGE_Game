df83689658f6b95de53b6692420d115c65fd0a4d
Martin Dahl Sørensen <Zexyqag@gmail.com>
2023-05-04 23:49:23 +0200

    Update User.cs

--- Assets/Scripts/API/Models/User.cs
+++ Assets/Scripts/API/Models/User.cs
@@ -7,7 +7,7 @@ namespace GAAUBAGE_Game.API.Models
         public string Alias { get; set; } = string.Empty;
         public string Tag { get; set; } = string.Empty;
         public List<Guid> ContextClassifications { get; set; } = new List<Guid>();
-        public List<Guid> BackgroundContexts { get; set; } = new List<Guid>();
+        public List<Guid> BackgroundClassifications { get; set; } = new List<Guid>();
         public List<Guid> SubImageGroups { get; set; } = new List<Guid>();
         public List<Guid> TrashSuperCategories { get; set; } = new List<Guid>();
         public List<Guid> TrashSubCategories { get; set; } = new List<Guid>();
