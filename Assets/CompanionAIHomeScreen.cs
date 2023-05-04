c14f4ccaf3c2223ab79c14e01b42d44d308967d1
Marcus <mhytte18@student.aau.dk>
2023-05-04 08:51:51 +0200

    Update CompanionAIHomeScreen.cs and Narrative_Home.unity

--- Assets/CompanionAIHomeScreen.cs
+++ Assets/CompanionAIHomeScreen.cs
@@ -6,27 +6,53 @@ using System;
 public class CompanionAIHomeScreen : MonoBehaviour
 {
     public TMPro.TMP_Text AIFace, AISpeech;
-    List<string> faces = new List<string> { "-  ° o °", "-  ° v °", "-  ° > °" };
-    List<string> speech = new List<string> { "Hello", "How are you doing?", "Ready to work?", "Good Morning", "Good evening", "Good Evening" };
+    List<string> faces = new List<string> { "  ° o °", "  ° v °", "  ° > °" };
+    List<string> speech = new List<string> { "", "Hello", "How are you doing?", "Ready to work?", "Good Morning", "Good evening", "Good Evening" };
     private void Start() {
         int selector = UnityEngine.Random.Range(0, speech.Count);
+        var text = string.Empty;
 
-
-        if (selector >= speech.Count - 3) {
+        if (selector == 0) {
+            switch (PlayerPrefs.GetInt("Level")) {
+                default:
+                    text = "Remember to train my model.";
+                    break;
+                case 1:
+                    text = "";
+                    break;
+                case 2:
+                    text = "";
+                    break;
+                case 3:
+                    text = "";
+                    break;
+                    //case 4:
+                    //    break;
+            }
+        } else if (selector >= speech.Count - 3) {
             switch (DateTime.Now.Hour) {
                 case < 12:
-                    AISpeech.text = speech[speech.Count - 3];
+                    text = speech[speech.Count - 3];
                     break;
                 default:
-                    AISpeech.text = speech[speech.Count - 2];
+                    text = speech[speech.Count - 2];
                     break;
                 case > 18:
-                    AISpeech.text = speech[speech.Count - 1];
+                    text = speech[speech.Count - 1];
                     break;
             }
         } else {
-            AISpeech.text = speech[selector];
+            text = speech[selector];
         }
         AIFace.text = faces[UnityEngine.Random.Range(0, faces.Count)];
+        StartCoroutine(PlayText(text));
+    }
+
+    IEnumerator PlayText(string speech) {
+        yield return new WaitForSeconds(4f);
+        foreach (char c in speech) {
+            AISpeech.text += c;
+            yield return new WaitForSeconds(.05f);
+        }
     }
 }
