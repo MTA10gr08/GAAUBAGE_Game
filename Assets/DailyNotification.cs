f2a9207213cce1f5dee8fdcbae90e73ccc30ce84
Marcus <Marcus@PP.local>
2023-05-08 15:42:20 +0200

    Update DailyNotification.cs, UserCreationScene.unity, and Endpoints.cs

--- Assets/DailyNotification.cs
+++ Assets/DailyNotification.cs
@@ -31,7 +31,7 @@ public class DailyNotification : MonoBehaviour
             });
         };
 
-        SendOrUpdateNotofication();
+        //SendOrUpdateNotofication();
 
         AndroidNotificationCenter.OnNotificationReceived += (data) =>
         {
